86e9c08e7e4f6a3f583b2739e1f145459d5eb376
Tony Pham <tony.p@lashowroom.com>
2020-12-27 21:54:27 -0800

    made components for the features of this project, wire up the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
-
-const PageOne = () => {
-    return <div>Page one</div>;
-};
-
-const PageTwo = () => {
-    return <div>Page two</div>;
-};
-
+import {BrowserRouter, Route, Link} from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return(
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne}/>
-                    <Route path="/" exact component={PageOne}/>
-                    <Route path="/pagetwo" component={PageTwo}/>
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
