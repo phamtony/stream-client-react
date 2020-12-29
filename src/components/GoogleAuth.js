9edfb06b2de855ac19637ced0481ed1361f8911d
Tony Pham <tony.p@lashowroom.com>
2020-12-28 22:55:49 -0800

    event listener functions making google sign in buttons work

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -20,13 +20,31 @@ class GoogleAuth extends React.Component {
         this.setState({isSignedIn: this.auth.isSignedIn.get()});
     };
 
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>Don't know if we are signed in</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in!</div>
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon"></i>
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>I am not signed in</div>
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon"></i>
+                    Sign In with Google
+                </button>
+            );
         }
     };
 
