8c7c60e24a78f3bc843fced0459a85a89af2a6ba
Tony Pham <tony.p@lashowroom.com>
2021-01-14 00:14:13 -0800

    make modal usable, integrate react.fragment

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Modal = () => {
+const Modal = (props) => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                Hello there it's a modal.
+        <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+            <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+                <div className="header">{props.title}</div>
+                <div className="content">
+                    {props.content}
+                </div>
+                <div className="actions">
+                    {props.actions}
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')
