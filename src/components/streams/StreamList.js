f86a99cad739f994f0bc3f135fa0a950441533e2
Tony Pham <tony.p@lashowroom.com>
2021-01-22 23:00:49 -0800

    rendered stream list, added reducer in combine reducers

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,42 @@
 import React from "react";
+import {connect} from "react-redux";
+import {fetchStreams} from "../../actions";
 
-const StreamList = () => {
-    return <div>StreamList</div>;
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams()
+    }
+
+    renderLists() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera"></i>
+                    <div className="content">
+                        {stream.title}
+                    </div>
+                    <div className="description">
+                        {stream.description}
+                    </div>
+                </div>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderLists()}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {streams: Object.values(state.streams)};
 };
 
-export default StreamList;
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
